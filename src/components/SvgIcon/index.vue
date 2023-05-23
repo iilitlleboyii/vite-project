c61b49f6cf1225b979e1f9c04cbead7c1625e9bd
iilitlleboyii <405422114@qq.com>
2023-05-23 16:51:32 +0800

    feat: add svg

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <svg aria-hidden="true" :style="{ width, height }">
+      <use :xlink:href="xlink_href" :fill="color"></use>
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    iconName: string
+    color?: string
+    width?: string
+    height?: string
+  }>(),
+  {
+    color: 'red',
+    width: '20px',
+    height: '20px',
+  },
+)
+
+const xlink_href = computed(() => {
+  return '#icon-' + props.iconName
+})
+</script>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
 // 引入element-plus插件及样式
@@ -8,9 +7,15 @@ import 'element-plus/dist/index.css'
 // 配置element-plus国际化
 // @ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// 配置SVG
+import 'virtual:svg-icons-register'
+
+// 引入自定义插件对象，注册整个项目全局组件
+import globalComponent from '@/components'
 
 const app = createApp(App)
 app.use(ElementPlus, {
   locale: zhCn,
 })
+app.use(globalComponent)
 app.mount('#app')
