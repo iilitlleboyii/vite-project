63c70469af4f89236ad88dc16b2e10a63242c4a2
iilitlleboyii <405422114@qq.com>
2023-05-26 17:50:53 +0800

    style: ignore

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -3,8 +3,6 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import piniaPluginRouter from 'pinia-plugin-router'
 import router from '@/router'
 
-
-
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 pinia.use(piniaPluginRouter(router))
